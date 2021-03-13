874e4e9e142347d2444e5db2a54c70f0133aa325
leonel-carvajal <liosoft777@gmail.com>
2021-03-13 13:08:12 -0600

    menu mobile

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const headerItems = document.querySelectorAll('.header__item')
+const iconHam = document.getElementById('header__img')
+const headerMenu = document.getElementById('header__menu')
+
+headerItems.forEach(item => {
+  item.addEventListener('click', (e) => {
+    e.target.nextElementSibling.classList.toggle('animation')
+    e.target.children[0].classList.toggle('rotate')
+  })
+})
+iconHam.addEventListener('click', (e) => {
+  if (!headerMenu.classList.contains('show')) {
+    iconHam.src = 'images/icon-close.svg'
+    headerMenu.classList.add('show')
+  } else {
+    headerMenu.classList.remove('show')
+    iconHam.src = 'images/icon-hamburger.svg'
+  }
+})
